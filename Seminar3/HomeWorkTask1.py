23071152bbf03216fc2fdb1784fc0cc73970dc48
ValSGav <valentin-s-gavrilov@yandex.ru>
2023-04-17 17:41:09 +0300

    HomeWork Sem3 ++

---
+++ Seminar3/HomeWorkTask1.py
@@ -0,0 +1,28 @@
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N]. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+# *Пример:*
+
+# 5
+#     1 2 3 4 5
+#     3
+#     -> 1
+
+def get_numb_occur_digit_in_array(number, arrray_of_numbs):
+    numb_of_occur = 0
+    for i in arrray_of_numbs:
+        if i == number:
+            numb_of_occur += 1
+    return numb_of_occur
+
+numb_of_elements = int(input("Введите количество элементов в массиве: "))
+
+array_of_numbers = []
+
+for i in range(numb_of_elements):
+    array_of_numbers.append(int(input(f"Введите число для {i} элемента массива: ")))
+
+number = int(input("Введите число, количество вхождений которого мы будем искать в массиве: "))
+
+print(f"Количество вхождений числа {number} в массив {array_of_numbers} - {get_numb_occur_digit_in_array(number, array_of_numbers)} раз")
---
+++ Seminar3/HomeWorkTask2.py
@@ -0,0 +1,65 @@
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+# *Пример:*
+
+# 5
+#     1 2 3 4 5
+#     6
+#     -> 5
+
+
+def print_the_number_closest_in_value_in_the_array(number, arrray_of_numbs):
+
+    curr_min_diff = 0
+    curr_min_negative_diff = 0
+
+    for i in array_of_numbers:
+
+        difference = i - number
+
+        if difference == 0:
+            print(
+                f"в массиве {array_of_numbers} ближайшим к нашему числу {number} являются оно само: {number}")
+            return
+        elif difference > 0:
+            if curr_min_diff == 0:
+                curr_min_diff = difference
+            else:
+                curr_min_diff = min(curr_min_diff, difference)
+        else:
+            if curr_min_negative_diff == 0:
+                curr_min_negative_diff = difference
+            else:
+                curr_min_negative_diff = max(
+                    curr_min_negative_diff, difference)
+
+    print(f'{curr_min_diff} и {curr_min_negative_diff}')
+
+    if curr_min_diff == -curr_min_negative_diff:
+        print(
+            f"в массиве {array_of_numbers} ближайшими к нашему числу {number} являются {number + curr_min_negative_diff} и {number + curr_min_diff}")
+    elif curr_min_diff == 0 or curr_min_negative_diff == 0:
+        print(
+            f"в массиве {array_of_numbers} ближайшим к нашему числу {number} являются {0}")
+    elif curr_min_diff < - curr_min_negative_diff:
+        print(
+            f"в массиве {array_of_numbers} ближайшим к нашему числу {number} являются {number + curr_min_diff}")
+    else:
+        print(
+            f"в массиве {array_of_numbers} ближайшим к нашему числу {number} являются {number + curr_min_negative_diff}")
+
+
+numb_of_elements = int(input("Введите количество элементов в массиве: "))
+
+array_of_numbers = []
+
+for i in range(numb_of_elements):
+    array_of_numbers.append(
+        int(input(f"Введите число для {i} элемента массива: ")))
+
+number = int(
+    input("Введите число, для которого мы будем искать самое близкое в массиве: "))
+
+print_the_number_closest_in_value_in_the_array(number, array_of_numbers)
