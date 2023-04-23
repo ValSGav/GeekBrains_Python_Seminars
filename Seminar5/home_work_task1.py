b10624f81eda5057656cb47f3b358b5e736d20da
Val Gavrilov <valentin-s-gavriliv@yandex.ru>
2023-04-23 15:11:05 +0300

    Home work seminar 5  add

--- Seminar5/home_work_task1.py
+++ Seminar5/home_work_task1.py
@@ -2,11 +2,13 @@
 # два числа A и B, и возводит число А в целую степень B с
 # помощью рекурсии.
 
+
 def my_pow(numb: int, pow: int) -> int:
     if pow == 0:
         return 1
     else:
         return numb * my_pow(numb, pow - 1)
+    
 
 numb = int(input("Введите число: "))
 pow_of_numb = int(input("Введите значение степени в которую нужно возвести число: "))
